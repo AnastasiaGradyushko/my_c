818fd2d8dbc9cc3900a8a645a4285eb864ba22c8
AnastasiaGradyushko <62464107+AnastasiaGradyushko@users.noreply.github.com>
2020-03-26 15:19:54 +0300

    Add files via upload

---
+++ 26.03. 4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#define M 3 /*סענמך*/
+#define N 4 /*סעמכבצמג*/
+
+int main()
+{
+  int a[M][N];
+  int i, j, k = 0;
+  int min = INT_MAX, jmin;
+
+    for(i = 0; i < M; i++)
+    for(j = 0; j < N; j++)
+      a[i][j] = rand()%100;
+
+  for(j = 0; j < N; j++){
+    for(i = 0; i < M; i++)
+        if (a[i][j] >= 0){
+        if (a[i][j] <= min){
+            min = a[i][j];
+            jmin = j;
+            k++;
+            }
+        }
+
+    if (k !=0)
+        printf("minimal %d -go stolbtsa = %d\n", jmin, min);
+    else printf("ops\n");
+  }
+
+
+  for(i = 0; i < M; i++){
+      for(j = 0; j < N; j++)
+        printf("%3d", a[i][j]);
+      printf("\n");
+
+  }
+  return 0;
+}
